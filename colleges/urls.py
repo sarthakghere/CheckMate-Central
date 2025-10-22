f5ea79fb1b2dcbdce8ef789953d9f216aaf88045
sarthakghere <sg622857@outlook.com>
2025-10-22 17:11:36 +0530

    URLS improved

--- colleges/urls.py
+++ colleges/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "colleges"
 
 urlpatterns = [
-    path("dashboard/college/", views.college_dashboard, name="college_dashboard"),
-    path("colleges/register/", views.register_college, name="register_college"),
-    path("staff/college/<int:college_id>/manage/", views.manage_college, name="manage_college"),
-    path("staff/college/<int:college_id>/api-key/", views.reset_api_key, name="reset_api_key"),
+    path("dashboard/", views.college_dashboard, name="college_dashboard"),
+    path("register/", views.register_college, name="register_college"),
+    path("<int:college_id>/", views.manage_college, name="manage_college"),
+    path("<int:college_id>/api-key/", views.reset_api_key, name="reset_api_key"),
 
 ]
--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("login/", views.email_password_login, name="login"),
     path("otp-verify/", views.otp_verify, name="otp_verify"),
     path("resend-otp/", views.resend_otp, name="resend_otp"),
-    path("dashboard/staff/", views.staff_dashboard, name="staff_dashboard"),
+    path("dashboard/", views.staff_dashboard, name="staff_dashboard"),
     path("logout/", views.user_logout, name="logout"),
 ]
 
