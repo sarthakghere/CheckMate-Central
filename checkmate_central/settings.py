8e29969fea54289dded835f518ab62ec71e059e0
sarthakghere <sg622857@outlook.com>
2025-10-24 21:59:08 +0530

    Implemented Persistent Storage for Media Files

--- checkmate_central/settings.py
+++ checkmate_central/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 
 # Default primary key field type
